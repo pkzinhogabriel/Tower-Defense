198cb139fa73779393bd630f3b539e548df76c72
pkzinhogabriel <pkzinhogabriel@gmail.com>
2024-10-22 14:39:40 -0300

    implementei mais algumas coisas

--- Tower Defense/Assets/Scripts/EnemySpawner.cs
+++ Tower Defense/Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -9,24 +12,47 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    public static UnityEvent onEnemyDestroy;
+
     private int currentWave = 1;
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftToSpawn;
     private bool isSpawning = false;
+    private void Awake()
+    {
+        onEnemyDestroy.AddListener(EnemyDestroyed);
+    }
+    private void Start()
+    {
+        StartWave();
+    }
     private void Update()
     {
         if (!isSpawning) return;
         timeSinceLastSpawn += Time.deltaTime;
-        if (timeSinceLastSpawn >= (1f / enemiesPerSecond))
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
         {
-            Debug.Log("Spawn Enemy");
+            SpawnEnemy();
+            enemiesLeftToSpawn--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f;
         }
+    }
+    private void EnemyDestroyed()
+    {
+        enemiesAlive--;
     }
     private void StartWave()
     {
         isSpawning = true;
         enemiesLeftToSpawn = EnemiesPerWave();
+    }
+    private void SpawnEnemy()
+    {
+        GameObject prefabToSpawn = enemyPrefabs[0];
+        Instantiate(prefabToSpawn, LevelManager.instance.startPoint.position, Quaternion.identity);
     }
     private int EnemiesPerWave()
     {
