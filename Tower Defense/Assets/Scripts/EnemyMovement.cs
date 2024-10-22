66d1de1afa7cab5c4e161aef6f82fdcabfc86ae5
pkzinhogabriel <pkzinhogabriel@gmail.com>
2024-10-22 13:59:34 -0300

    terminei o script do movimento do inimigo

---
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 2f;
+    private Transform target;
+    private int pathIndex = 0;
+    // Start is called before the first frame update
+   private void Start()
+    {
+        target = LevelManager.instance.path[pathIndex];
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(target.position,transform.position) <= 0.1f)
+        {
+            pathIndex++;
+
+            if (pathIndex == LevelManager.instance.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.instance.path[pathIndex];
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position -  transform.position).normalized;
+
+        rb.velocity = direction * moveSpeed;
+    }
+}
