cccd2a28fa5ff959a49c132509717b80f912f979
pkzinhogabriel <pkzinhogabriel@gmail.com>
2024-10-23 10:28:00 -0300

    aprimorei o script Turret e adicionei um Debug.Log pra ver se pelo menos está atirando

--- Tower Defense/Assets/Scripts/Turret.cs
+++ Tower Defense/Assets/Scripts/Turret.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
     [SerializeField] private float targetingRange = 5f;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    [SerializeField] private float bps = 1f;
     private Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timeUntilFire;
+    
 
     // Update is called once per frame
    private void Update()
@@ -21,6 +22,28 @@ public class Turret : MonoBehaviour
             FindTarget();
             return;
         }
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        } 
+       else
+        {
+            timeUntilFire += Time.deltaTime;
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+        
+    }
+    private void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
     }
     private void FindTarget()
     {
