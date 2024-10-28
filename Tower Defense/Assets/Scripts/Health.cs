2529883165e83e82c5ed6221a2560ed9c8084b45
pkzinhogabriel <pkzinhogabriel@gmail.com>
2024-10-28 13:41:07 -0300

    criei o script que precisava e fiz as modificações pra quando matar o inimigo ganhar Moedinhas

--- Tower Defense/Assets/Scripts/Health.cs
+++ Tower Defense/Assets/Scripts/Health.cs
@@ -6,7 +6,7 @@ public class Health : MonoBehaviour // Classe Health: Gerencia os pontos de vida
 
 {
     [SerializeField] protected float hitPoints = 2;    // Pontos de vida iniciais do objeto. 
-
+    [SerializeField] private int currencyWorth = 50;
     protected bool isDestroyed = false;    // Indica se o objeto já foi destruído.
 
     public virtual void TakeDamage(float dmg)    // Método virtual para aplicar dano ao objeto.
@@ -14,9 +14,9 @@ public class Health : MonoBehaviour // Classe Health: Gerencia os pontos de vida
     {
         hitPoints -= dmg;// Subtrai o dano dos pontos de vida.
         if (hitPoints <= 0 && !isDestroyed)        // Verifica se os pontos de vida chegaram a zero ou menos e se o objeto não foi destruído.
-
         {
             EnemySpawner.onEnemyDestroy.Invoke();// Notifica o spawner que um inimigo foi destruído.
+            LevelManager.instance.IncreaseCurrency(currencyWorth);
             isDestroyed = true;// Marca o objeto como destruído.
             Destroy(gameObject);// Destrói o objeto do jogo.
         }
--- Tower Defense/Assets/Scripts/LevelManager.cs
+++ Tower Defense/Assets/Scripts/LevelManager.cs
@@ -11,9 +11,32 @@ public class LevelManager : MonoBehaviour // Classe LevelManager: Gerencia infor
 
     public Transform[] path;    // Array de pontos que definem o caminho que os inimigos devem seguir.
 
+    public int currency; // variável das Moedas 
+
     private void Awake()    // Método chamado quando o objeto é inicializado.
 
     {
         instance = this; // Inicializa a instância singleton.
+    }
+    private void Start()
+    {
+        currency = 100; // inicia com 100 moedas
+    }
+    public void IncreaseCurrency(int amount) // Metodo que aumenta as Moedinhas
+    {
+        currency += amount;
+    }
+    public bool SpendCurrency(int amount)
+    {
+        if (amount <= currency)
+        {
+            currency -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.Log("Pobre você é");
+            return false;        
+        }
     }
 }
